ba3a7d421696d0ee45f5bf57f2f6ff486376719c
Júlio Pinto <pinto.julioj03@gmail.com>
2023-12-20 19:15:14 +0000

    feat: add bools

--- ia/orders/orders.py
+++ ia/orders/orders.py
@@ -3,20 +3,30 @@ from ia.map.place import Place
 
 class Order:
 
-    def __init__(self, time, place):
+    def __init__(self, time, place : Place):
         self.id = uuid.uuid1()
         self.time = time
         self.place = place
         self.products = dict({})
         self.rating = 0
         self.weight = 0
+        self.todeliver = False
+        self.delivered = False
 
-    def add_product(self, product):
-        self.products.add(product)
-        self.weight += product.weight
+    def add_product(self, product : Product):
+        if not self.todeliver:
+            self.products.add(product)
+            self.weight += product.weight
 
     def set_rating(self, rating):
-        self.rating = rating
+        if not self.delivered:
+            self.rating = rating
+
+    def askDelivery(self):
+        self.done = True
+
+    def deliver(self):
+        self.delivered = True
 
             
 
