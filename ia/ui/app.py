c007f68e35af523474ea74f266fab6c25fca2feb
Carlos-oficial <carlosribeiro2003oficial@gmail.com>
2023-12-22 04:07:43 +0000

    rename nodes (:

--- ia/ui/app.py
+++ ia/ui/app.py
@@ -17,7 +17,7 @@ class State:
     def __init__(self):
         self.G = None
         self.gdf = None
-        self.selected_nodes = []
+        self.selected_nodes = dict({})
         self.location = ""
 
     def save_to_file(self, fpath):
@@ -54,7 +54,12 @@ class MapGenerator:
         self.points_button = tk.Button(
             master, text="Randomize points", command=self.select_nodes
         )
-        self.points_button.grid(row=2)
+        self.points_button.grid(row=2, column=0)
+
+        self.name_nodes_button = tk.Button(
+            master, text="Name points", command=self.name_nodes
+        )
+        self.name_nodes_button.grid(row=2, column=1)
 
         self.save_button = tk.Button(master, text="Save", command=self.save)
         self.save_button.grid(row=3)
@@ -64,7 +69,7 @@ class MapGenerator:
 
     def retrieve_map(self):
         try:
-            self.state.selected_nodes = []
+            self.state.selected_nodes = dict({})
             # Get location from the entry
             self.state.location = self.location_entry.get()
 
@@ -91,7 +96,16 @@ class MapGenerator:
             centrality = nx.degree_centrality(self.state.G)
 
             # Get the nodes with highest centrality
-            self.state.selected_nodes = random.choices(list(self.state.G), k=10)
+            def take1(l):
+                elem = random.choice(l)
+                l.remove(elem)
+                return elem
+
+            letters = list(string.ascii_uppercase)
+            self.state.selected_nodes = {
+                k: take1(letters) for k in random.choices(list(self.state.G), k=10)
+            }
+
             self.master.update_idletasks()
             self.draw()
         except Exception as e:
@@ -99,10 +113,51 @@ class MapGenerator:
             error_label = ttk.Label(self.master, text=e)
             error_label.grid(row=0, column=0)
 
+    def name_nodes(self):
+        form = tk.Toplevel(self.master)
+        form.title("Rename Nodes")
+        labels = dict({})
+        entries = dict({})
+        for node, name in self.state.selected_nodes.items():
+            labels[node] = tk.Label(form, text=name)
+            labels[node].grid(padx=20, pady=5)
+            entries[node] = tk.Entry(form)
+            entries[node].grid(padx=20, pady=5)
+
+        # Function to handle form submission
+        def submit_form():
+            for node, entry in entries.items():
+                val = entry.get()
+                if val:
+                    self.state.selected_nodes[node] = val
+            self.update_node_names()
+            # Close the form
+            form.destroy()
+            self.draw()
+
+        # Add a submit button to the form
+        submit_button = tk.Button(form, text="Submit", command=submit_form)
+        submit_button.grid(pady=20)
+
+    def update_node_names(self):
+        pos = {
+            node: (data["x"], data["y"]) for node, data in self.state.G.nodes(data=True)
+        }
+        nodes = nx.draw_networkx_nodes(
+            self.state.G,
+            pos=pos,
+            nodelist=self.state.selected_nodes.keys(),
+            node_color="r",
+            node_size=50,
+        )
+        for node, data in self.state.G.nodes(data=True):
+            if node in self.state.selected_nodes.keys():
+                data["pickup"] = self.state.selected_nodes[node]
+
     def save(self):
         # Function to create a new form (Toplevel window)
         form = tk.Toplevel(self.master)
-        form.title("Input Form")
+        form.title("Save")
 
         # Add entry widgets to the form
         label_fpath = tk.Label(form, text="where to save?")
@@ -148,30 +203,17 @@ class MapGenerator:
             self.state.gdf, ax=self.ax, alpha=0.4, show=False, color="cyan"
         )
 
-        pos = {
-            node: (data["x"], data["y"]) for node, data in self.state.G.nodes(data=True)
-        }
-        nodes = nx.draw_networkx_nodes(
-            self.state.G,
-            pos=pos,
-            nodelist=self.state.selected_nodes,
-            node_color="r",
-            node_size=50,
-        )
-        a = list(string.ascii_uppercase)
-        i = 0
-        for node, data in self.state.G.nodes(data=True):
-            if node in self.state.selected_nodes:
-                data["pickup"] = a[i % len(a)] + (
-                    str(int(i / len(a))) if ((i / len(a)) >= 1) else ""
-                )
-                i += 1
+        self.update_node_names()
 
         node_labels = nx.get_node_attributes(self.state.G, "pickup")
         labels = {
             key: node_labels[key]
-            for key in self.state.selected_nodes
+            for key in self.state.selected_nodes.keys()
             if key in node_labels
+        }
+
+        pos = {
+            node: (data["x"], data["y"]) for node, data in self.state.G.nodes(data=True)
         }
 
         self.network_lables = nx.draw_networkx_labels(
@@ -187,7 +229,6 @@ class MapGenerator:
 
         if self.canvas is None:
             self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
-            # self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=1)
             self.canvas.get_tk_widget().grid()
         else:
             # Update the existing canvas with the new figure
